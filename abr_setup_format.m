54f7db96c47637a5d5e575b4d6f436b42aaa1ecf
Local Admin <Heinz Admin@HHSSLHS-4H24H03.BoilerAD.Purdue.edu>
2022-07-25 16:16:29 -0400

    restored setup files to generic setup files

--- abr_setup_format.m
+++ abr_setup_format.m
@@ -18,12 +18,12 @@ if (ismac == 1) %Use if using a MAC
     
     
 else %Use if using Windows/Linux
-    addpath(genpath('C:\Users\ischwein\Desktop\For_Purdue_Labs\Heinz_LabData')) %ENTER the path of the directory containing your 'ABRAnalysis' folder
-    addpath(genpath('Z:\Projects')) %ENTER the path of the directory containing your project folder
-    code_DIR='Z:\Projects\Ivy_Andrew_CARBOvTTS\Code Archive\ABRanalysis-master'; %ENTER the path of your 'ABRAnalysis' folder
-    project_DIR='Z:\Projects\Ivy_Andrew_CARBOvTTS\'; %ENTER the path of your project folder
+    addpath(genpath('Add code directory')) %ENTER the path of the directory containing your 'ABRAnalysis' folder
+    addpath(genpath('Add project directory')) %ENTER the path of the directory containing your project folder
+    code_DIR='Add code directory'; %ENTER the path of your 'ABRAnalysis' folder
+    project_DIR='Add project directory'; %ENTER the path of your project folder
 
-    abr_root_dir = [code_DIR '\ABR-master\Matlab_ABR\'];
+    abr_root_dir = [code_DIR 'ABR-master\Matlab_ABR\'];
     
     abr_data_dir = [project_DIR 'Data\'];
     abr_out_dir = [project_DIR 'Analysis\ABR\'];
