f6b831ec5233d8b803dc998dcb87ff3633022c5f
sivaprakasaman <andrew.n.sivaprakasam@gmail.com>
2023-06-29 15:10:25 -0400

    minor changes...checking windows compatibility

--- abr_setup.m
+++ abr_setup.m
@@ -5,10 +5,15 @@ global abr_root_dir abr_data_dir abr_out_dir
 addpath(genpath(pwd)) %ENTER the path of the directory containing your 'ABRAnalysis' folder
 rmpath(genpath('Trash'));
 
+%linux
 addpath(genpath('/media/sivaprakasaman/AndrewNVME/Pitch_Study/Pitch_Diagnostics_SH_AS/ABR/Chin/')) %ENTER the path of the directory containing your project folder
 project_DIR='/media/sivaprakasaman/AndrewNVME/Pitch_Study/Pitch_Diagnostics_SH_AS/ABR/Chin/Baseline/'; %ENTER the path of your project folder
 
-code_DIR=[pwd,'/']; %ENTER the path of your 'ABRAnalysis' folder
+% %windows
+% addpath(genpath('A:/Pitch_Study/Pitch_Diagnostics_SH_AS/ABR/Chin/')) %ENTER the path of the directory containing your project folder
+% project_DIR='A:/Pitch_Study/Pitch_Diagnostics_SH_AS/ABR/Chin/Baseline/'; %ENTER the path of your project folder
+
+% code_DIR=[pwd,'/']; %ENTER the path of your 'ABRAnalysis' folder
 code_DIR = pwd;
 abr_root_dir = [code_DIR];
 abr_data_dir = [project_DIR];
